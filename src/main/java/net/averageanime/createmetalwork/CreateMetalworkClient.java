69b4afbf96a1f7f932db2f7cdb496145b0252b68
averageanime <nkarmartsangschool@gmail.com>
2024-01-21 11:46:34 -0800

    asset updates

--- src/main/java/net/averageanime/createmetalwork/CreateMetalworkClient.java
+++ src/main/java/net/averageanime/createmetalwork/CreateMetalworkClient.java
@@ -18,19 +18,16 @@ public class CreateMetalworkClient implements ClientModInitializer {
 // Vanilla Materials
 
         FluidRenderHandlerRegistry.INSTANCE.register(ModFluids.STILL_MOLTEN_IRON, ModFluids.FLOWING_MOLTEN_IRON, new SimpleFluidRenderHandler(
-                new Identifier("createmetalwork:block/molten_ore_still"),
-                new Identifier("createmetalwork:block/molten_ore_flow"),
-                0xf0c77b
+                new Identifier("createmetalwork:block/molten_iron_still"),
+                new Identifier("createmetalwork:block/molten_iron_flow")
         ));
         FluidRenderHandlerRegistry.INSTANCE.register(ModFluids.STILL_MOLTEN_COPPER, ModFluids.FLOWING_MOLTEN_COPPER, new SimpleFluidRenderHandler(
-                new Identifier("createmetalwork:block/molten_ore_still"),
-                new Identifier("createmetalwork:block/molten_ore_flow"),
-                0xed7e00
+                new Identifier("createmetalwork:block/molten_copper_still"),
+                new Identifier("createmetalwork:block/molten_copper_flow")
         ));
         FluidRenderHandlerRegistry.INSTANCE.register(ModFluids.STILL_MOLTEN_GOLD, ModFluids.FLOWING_MOLTEN_GOLD, new SimpleFluidRenderHandler(
-                new Identifier("createmetalwork:block/molten_ore_still"),
-                new Identifier("createmetalwork:block/molten_ore_flow"),
-                0xffd300
+                new Identifier("createmetalwork:block/molten_gold_still"),
+                new Identifier("createmetalwork:block/molten_gold_flow")
         ));
         FluidRenderHandlerRegistry.INSTANCE.register(ModFluids.STILL_MOLTEN_NETHERITE, ModFluids.FLOWING_MOLTEN_NETHERITE, new SimpleFluidRenderHandler(
                 new Identifier("createmetalwork:block/molten_ore_still"),
